5bece2ff6e85c3594d3068e70f8da319007df356
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2020-04-16 12:42:44 +0200

    Details page not functioning.

---
+++ frontend/views/wenetapp/details.php
@@ -0,0 +1,19 @@
+<?php
+    $this->title = Yii::$app->name . ' | ' . Yii::t('common', 'App');
+    $this->params['breadcrumbs'][] = Yii::t('common', 'App');
+?>
+
+
+<!-- <script async src="https://telegram.org/js/telegram-widget.js?8" data-telegram-login="uh_test_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script type="text/javascript">
+  function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+</script> -->
+
+puppa
+<?php
+
+    // print_r($app);
+
+ ?>
