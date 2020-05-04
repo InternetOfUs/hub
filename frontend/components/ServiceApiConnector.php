e1a113f34c667679998aa95452b37c4755303773
Carlo Caprini <carlo.caprini@u-hopper.com>
2020-05-04 17:55:24 +0200

    Removed duplicated function

--- frontend/components/ServiceApiConnector.php
+++ frontend/components/ServiceApiConnector.php
@@ -27,35 +27,4 @@ class ServiceApiConnector extends BaseConnector {
         ];
     }
 
-    private function post($url, $headers, $body=null) {
-        $headers[] = 'Content-Type: application/json';
-        try {
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            if ($body) {
-                curl_setopt($ch, CURLOPT_POSTFIELDS, JSON::encode($body));
-            }
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            $result = curl_exec($ch);
-            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-
-            if ($status == 200 || $status == 201) {
-                return $result;
-            } else {
-                $log = 'Received error response with ['.$status.'] for POST query to ['.$url.']: '.$result;
-                Yii::warning($log);
-                throw new \Exception($log);
-            }
-        } catch (\Exception $e) {
-            $log = 'Something went wrong while running POST query to ['.$url.'] with body ' . JSON::encode($body);
-            Yii::error($log);
-            throw new \Exception($log, 0, $e);
-        }
-    }
-
 }
