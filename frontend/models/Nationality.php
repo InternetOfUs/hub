f65a0e31e660d75eeb357ab99c83ccff2de48c7d
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2020-05-04 18:12:44 +0200

    Fixed const and labels in Nationality model.

--- frontend/models/Nationality.php
+++ frontend/models/Nationality.php
@@ -6,27 +6,27 @@ use Yii;
 
 class Nationality {
 
-    const STATE_ITALY = "Italiana";
-    const STATE_SPAIN = "Española";
-    const STATE_ENGLAND = "English";
-    const STATE_DENMARK = "Dansk";
-    const STATE_ISREAL = "לאום ישראלי";
-    const STATE_PARAGUAI = "Paraguaya";
-    const STATE_INDIA = "भारतीय राष्ट्रीयता";
-    const STATE_MONGOLIA = "Монгол үндэстэн";
-    const STATE_MEXICO = "Mexicana";
+    const STATE_ITALY = "italy";
+    const STATE_SPAIN = "spain";
+    const STATE_ENGLAND = "england";
+    const STATE_DENMARK = "denmark";
+    const STATE_ISREAL = "israel";
+    const STATE_PARAGUAI = "paraguai";
+    const STATE_INDIA = "india";
+    const STATE_MONGOLIA = "mongolia";
+    const STATE_MEXICO = "mexico";
 
     public static function nationalityLabels() {
         return [
-            self::STATE_ITALY => self::STATE_ITALY,
-            self::STATE_SPAIN => self::STATE_SPAIN,
-            self::STATE_ENGLAND => self::STATE_ENGLAND,
-            self::STATE_DENMARK => self::STATE_DENMARK,
-            self::STATE_ISREAL => self::STATE_ISREAL,
-            self::STATE_PARAGUAI => self::STATE_PARAGUAI,
-            self::STATE_INDIA => self::STATE_INDIA,
-            self::STATE_MONGOLIA => self::STATE_MONGOLIA,
-            self::STATE_MEXICO => self::STATE_MEXICO
+            self::STATE_ITALY => "Italiana",
+            self::STATE_SPAIN => "Española",
+            self::STATE_ENGLAND => "English",
+            self::STATE_DENMARK => "Dansk",
+            self::STATE_ISREAL => "לאום ישראלי",
+            self::STATE_PARAGUAI => "Paraguaya",
+            self::STATE_INDIA => "भारतीय राष्ट्रीयता",
+            self::STATE_MONGOLIA => "Монгол үндэстэн",
+            self::STATE_MEXICO => "Mexicana"
         ];
     }
 
