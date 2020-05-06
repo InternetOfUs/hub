49ccd3ca717f990ff812a2ce3d57ed96c6cacb67
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2020-05-06 14:35:57 +0200

    Fixed bug.

--- frontend/controllers/PlatformController.php
+++ frontend/controllers/PlatformController.php
@@ -6,6 +6,7 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use frontend\models\AppPlatformTelegram;
+use frontend\models\UserAccountTelegram;
 use frontend\models\WenetApp;
 use frontend\models\AppPlatform;
 use yii\db\Query;
@@ -84,6 +85,8 @@ class PlatformController extends Controller {
         $model = AppPlatformTelegram::find()->where(["id" => $id])->one();
         $model->status = AppPlatform::STATUS_DELETED;
 
+        $users = UserAccountTelegram::find()->where(['app_id' => $model->app_id, 'active' => UserAccountTelegram::ACTIVE ])->all();
+
         $app = WenetApp::find()->where(['id' => $model->app_id])->one();
         $appPlatforms = $app->platforms();
         if ($app->status == WenetApp::STATUS_ACTIVE && count($appPlatforms) == 1) {
@@ -96,6 +99,18 @@ class PlatformController extends Controller {
             // TODO
             // Yii::error('Could not create new Wenet APP', '');
         } else {
+            $usersOk = true;
+            foreach ($users as $user) {
+                $user->active = UserAccountTelegram::NOT_ACTIVE;
+                if(!$user->save()){
+                    $usersOk = false;
+                }
+                if($usersOk == false){
+                    $transactionOk = false;
+                    break;
+                }
+            }
+
             if($appToDevMode){
                 if(!$app->save()){
                     $transactionOk = false;
@@ -104,7 +119,7 @@ class PlatformController extends Controller {
                 }
             }
         }
-        // $transactionOk = false;
+        
         if ($transactionOk) {
             if($appToDevMode){
                 Yii::$app->session->setFlash('warning', Yii::t('app', 'Because there are no platforms available for this app, the app has been automatically setted as "In development" mode.'));
