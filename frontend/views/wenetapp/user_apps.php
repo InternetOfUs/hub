f7cccbef88298f9b4e175f5bd474514e6a0450d2
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2020-05-06 18:24:43 +0200

    Fixed check for visualisation.

--- frontend/views/wenetapp/user_apps.php
+++ frontend/views/wenetapp/user_apps.php
@@ -6,9 +6,11 @@
     $this->title = Yii::$app->name . ' | ' . Yii::t('common', 'My Apps');
     $this->params['breadcrumbs'][] = Yii::$app->user->identity->username;
     $this->params['breadcrumbs'][] = Yii::t('common', 'My Apps');
+
+    $apps = Yii::$app->user->getIdentity()->apps;
 ?>
 
-<?php if (!WenetApp::thereAreActiveApps()) { ?>
+<?php if (count($apps) == 0) { ?>
     <div class="row">
     	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <p style="text-align:center; font-weight:400;">
@@ -21,8 +23,6 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     		<div class="userAppsContainer">
                 <?php
-                     $apps = Yii::$app->user->getIdentity()->apps;
-                     
                      foreach ($apps as $key => $app) {
                          $availablePlatforms = [];
                          if($app->hasPlatformTelegram()) {
@@ -52,7 +52,6 @@
 
                          echo $content;
                      }
-
                 ?>
             </div>
     	</div>
