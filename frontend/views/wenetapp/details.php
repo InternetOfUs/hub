cd1afb7b27ff86eaa7d4e798f49c43efd40f8c42
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2020-05-08 14:39:18 +0200

    In the details of an applications now is showed only the short name of the creator.

--- frontend/views/wenetapp/details.php
+++ frontend/views/wenetapp/details.php
@@ -18,7 +18,7 @@
     <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
         <h1><?php echo $app->name; ?></h1>
         <p style="margin:20px 0;"><?php echo $app->description; ?></p>
-        <p><strong><?php echo Yii::t('app', 'Creator'); ?>:</strong> <?php echo $app->owner->email; ?></p>
+        <p><strong><?php echo Yii::t('app', 'Creator'); ?>:</strong> <?php echo $app->getOwnerShortName(); ?></p>
         <?php
             if(count($app->associatedCategories) > 0){
                 $categories = '<ul class="tags_list">';
