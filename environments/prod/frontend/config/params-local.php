23a6517bca736a27de86ce431c499ac99437f8de
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2020-12-23 11:55:36 +0100

    Added params for email.

--- environments/prod/frontend/config/params-local.php
+++ environments/prod/frontend/config/params-local.php
@@ -12,4 +12,8 @@ return [
 	'kong.internal.url' => getenv('KONG_INTERNAL_URL'),
 	'kong.external.url' => getenv('KONG_EXTERNAL_URL'),
 	'kong.provision.key' => getenv('KONG_PROVISION_KEY'),
+
+	'elasticmail.api.key' => getenv('ELASTICMAIL_API_KEY'),
+    'email.from.name' => getenv('EMAIL_FROM_NAME'),
+	'email.from' => getenv('EMAIL_FROM'),
 ];
