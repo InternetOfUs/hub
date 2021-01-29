5c643363fe4bc6915bda670b92e1f725e21ff4bd
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2021-01-29 12:12:34 +0100

    Fixed wrong check on guest user.

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -11,26 +11,28 @@ class BaseController extends Controller {
     public function init(){
         parent::init();
 
-        $userProfile = Yii::$app->serviceApi->getUserProfile(Yii::$app->user->id);
+        if(!Yii::$app->user->isGuest){
+            $userProfile = Yii::$app->serviceApi->getUserProfile(Yii::$app->user->id);
 
-        $profileFieldsToCheck = [
-            'first_name',
-            'last_name',
-            'locale',
-            'gender'
-        ];
+            $profileFieldsToCheck = [
+                'first_name',
+                'last_name',
+                'locale',
+                'gender'
+            ];
 
-        $profileIsComplete = true;
-        foreach ($profileFieldsToCheck as $profileFieldToCheck) {
-            if($userProfile[$profileFieldToCheck] == '' || $userProfile[$profileFieldToCheck] == null ){
-                $profileIsComplete = false;
-                break;
+            $profileIsComplete = true;
+            foreach ($profileFieldsToCheck as $profileFieldToCheck) {
+                if($userProfile[$profileFieldToCheck] == '' || $userProfile[$profileFieldToCheck] == null ){
+                    $profileIsComplete = false;
+                    break;
+                }
             }
-        }
 
-        if(!$profileIsComplete && !Yii::$app->user->isGuest){
-            $content = Yii::t('profile', 'Remember to fill in your') .' '. Html::a( Yii::t('profile', 'profile'), ['/user/profile']) . '.';
-            Yii::$app->session->setFlash('error', $content);
+            if(!$profileIsComplete){
+                $content = Yii::t('profile', 'Remember to fill in your') .' '. Html::a( Yii::t('profile', 'profile'), ['/user/profile']) . '.';
+                Yii::$app->session->setFlash('error', $content);
+            }
         }
     }
 
