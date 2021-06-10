608ba60551fdbfab13c4224af5b7c619cd7faa20
Rossana Bartolacelli <rossana.bartolacelli@u-hopper.com>
2021-06-10 17:49:52 +0200

    Added migration for managing app with no task type.

--- console/migrations/m210608_083000_add_task_type_id_field_to_app_table.php
+++ console/migrations/m210608_083000_add_task_type_id_field_to_app_table.php
@@ -14,6 +14,7 @@ class m210608_083000_add_task_type_id_field_to_app_table extends Migration
     {
         $this->execute("ALTER TABLE `app` ADD `task_type_id` INT(11)  NULL  DEFAULT NULL  AFTER `image_url`;");
         $this->execute("ALTER TABLE `app` ADD FOREIGN KEY (`task_type_id`) REFERENCES `task_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;");
+        $this->execute("update app set status = 0 where task_type_id is NULL and status = 1;");
     }
 
     /**
